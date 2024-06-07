feda1f028532766887a3e9ecace0b4a20c8db897
karolbiskup <karolbiskup1234@gmail.com>
2024-06-07 12:01:01 +0200

    dodanie kolejnego napisu wyswietlajacego

--- my_proj/views.py
+++ my_proj/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Zaczynam tworzenie stron w django to jest moja strona po drobnych modyfikacjach")
+
+
+def caption_1(request):
+    return HttpResponse('Zaczynam tworzenie stron w django to jest moj drugi napis')
+
